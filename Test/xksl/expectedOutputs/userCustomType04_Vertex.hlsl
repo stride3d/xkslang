6609763ef94a3886d74696d774cabfc9b14727fc
Jonathan <riehl.jonathan@gmail.com>
2017-12-22 13:55:30 +0100

    Update the sample with more cases

--- Test/xksl/expectedOutputs/userCustomType04_Vertex.hlsl
+++ Test/xksl/expectedOutputs/userCustomType04_Vertex.hlsl
@@ -9,20 +9,32 @@ struct ShaderMain_CustomStruct
     float3 Color;
 };
 
-ShaderMain_CustomStruct ShaderMain_GetMainStruct()
+ShaderMain_CustomStruct ShaderMain_GetMainStruct1()
 {
     ShaderMain_CustomStruct c1;
     return c1;
 }
 
-ShaderBase_CustomStruct ShaderMain_GetBaseStruct()
+ShaderMain_CustomStruct ShaderMain_GetMainStruct2()
 {
-    ShaderBase_CustomStruct c1;
+    ShaderMain_CustomStruct c1;
     return c1;
 }
 
 void ShaderMain_aFunctionTakingMainStruct(ShaderMain_CustomStruct c)
 {
+}
+
+ShaderBase_CustomStruct ShaderMain_GetBaseStruct1()
+{
+    ShaderBase_CustomStruct c1;
+    return c1;
+}
+
+ShaderBase_CustomStruct ShaderMain_GetBaseStruct2()
+{
+    ShaderBase_CustomStruct c1;
+    return c1;
 }
 
 void ShaderMain_aFunctionTakingBaseStruct(ShaderBase_CustomStruct c)
@@ -37,14 +49,18 @@ ShaderBase_CustomStruct ShaderMain_ConvertStreamsToShaderBaseStreams(ShaderMain_
 
 void vert_main()
 {
-    ShaderMain_CustomStruct c1 = ShaderMain_GetMainStruct();
-    ShaderBase_CustomStruct c2 = ShaderMain_GetBaseStruct();
+    ShaderMain_CustomStruct c1 = ShaderMain_GetMainStruct1();
+    ShaderMain_CustomStruct c2 = ShaderMain_GetMainStruct2();
+    c1 = c2;
     ShaderMain_CustomStruct param = c1;
     ShaderMain_aFunctionTakingMainStruct(param);
-    ShaderBase_CustomStruct param_1 = c2;
+    ShaderBase_CustomStruct c3 = ShaderMain_GetBaseStruct1();
+    ShaderBase_CustomStruct c4 = ShaderMain_GetBaseStruct2();
+    c3 = c4;
+    ShaderBase_CustomStruct param_1 = c3;
     ShaderMain_aFunctionTakingBaseStruct(param_1);
     ShaderMain_CustomStruct param_2 = c1;
-    c2 = ShaderMain_ConvertStreamsToShaderBaseStreams(param_2);
+    c3 = ShaderMain_ConvertStreamsToShaderBaseStreams(param_2);
 }
 
 void main()
